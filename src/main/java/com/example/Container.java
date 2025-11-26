5bf9a5526b27db6cff154f83ec14324427098469
JayQust <gawsinor@gmail.com>
2025-11-26 14:16:28 +0300

    safe update :wq

--- src/main/java/com/example/Container.java
+++ src/main/java/com/example/Container.java
@@ -1,5 +1,56 @@
 package com.example;
 
-public class Container {
+public class Container<T> {
+    private Object[] data; //внутренний массив
+    private int size;      //текущее количество элементов
+    
 
+
+
+    /** hELP */
+    private Container() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    private Container(int initialCapacity) {
+        if (initialCapacity<=0) {
+            throw new IllegalArgumentException("initialCapacity must be > 0");
+        }
+        this.data = new Object[initialCapacity];
+        this.size = 0;
+    }
+
+    public void add(T item) {
+        
+    }
+
+    public T get(int index) {
+
+    }
+
+    public void remove(int index) {
+
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+        for (int i = 0; i<size; i++) {
+            data[i] = null;
+        }
+        size = 0;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", size: "+ size);
+        }
+    }
+
+    private void ensureCapacity() {
+        if (size == data.length) {
+            int newCapacity = data.length +}
+    }
 }
